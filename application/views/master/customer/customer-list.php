bc6f3dea4275840156d9f6d4e5950f6d4ccabef0
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-10-23 00:25:04 +0700

    Fixe error modal edit pelanggan

--- application/views/master/customer/customer-list.php
+++ application/views/master/customer/customer-list.php
@@ -110,7 +110,7 @@ $this->load->view('_partials/header');
 		<div class="modal-dialog modal-lg modal-dialog-centered">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Buat Pelanggan Baru</h5>
+					<h5 class="modal-title" id="exampleModalLabel">Edit Pelanggan</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
