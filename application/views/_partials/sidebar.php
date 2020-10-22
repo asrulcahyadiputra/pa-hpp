8a4005708ed6706fb6d629120b8e7ca03869ee9c
Asrul Cahyadi Putra <asrulcahyadiputra99@gmail.com>
2020-10-22 20:28:47 +0700

    Template Configuration

---
+++ application/views/_partials/sidebar.php
@@ -0,0 +1,46 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<div class="main-sidebar sidebar-style-2">
+	<aside id="sidebar-wrapper">
+		<div class="sidebar-brand">
+			<a href="<?php echo base_url('Dashboard'); ?>">PA HPP</a>
+		</div>
+		<div class="sidebar-brand sidebar-brand-sm">
+			<a href="<?php echo base_url('Dashboard'); ?>">PA</a>
+		</div>
+		<ul class="sidebar-menu">
+			<li class="<?php echo $this->uri->segment(1) == 'Dashboard' ? 'active' : ''; ?>">
+				<a href="#" class="nav-link"><i class="fas fa-home"></i><span>Dashboard</span></a>
+			</li>
+			<!-- ./dashboard -->
+			<li class="dropdown <?php echo $this->uri->segment(2) == 'layout_default' || $this->uri->segment(2) == 'layout_transparent' || $this->uri->segment(2) == 'layout_top_navigation' ? 'active' : ''; ?>">
+				<a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-server"></i> <span>Data Master</span></a>
+				<ul class="dropdown-menu">
+					<li class="<?php echo $this->uri->segment(2) == 'layout_default' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/layout_default">Pelanggan</a></li>
+					<li><a class="nav-link" href="<?php echo base_url(); ?>dist/layout_transparent">Produk</a></li>
+					<li><a class="nav-link" href="<?php echo base_url(); ?>dist/layout_top_navigation">Bahan Baku</a></li>
+					<li><a class="nav-link" href="<?php echo base_url(); ?>dist/layout_top_navigation">Bill of Materials (BOM)</a></li>
+				</ul>
+			</li>
+			<!-- ./data master -->
+			<li class="dropdown <?php echo $this->uri->segment(2) == 'features_activities' || $this->uri->segment(2) == 'features_post_create' || $this->uri->segment(2) == 'features_posts' || $this->uri->segment(2) == 'features_profile' || $this->uri->segment(2) == 'features_settings' || $this->uri->segment(2) == 'features_setting_detail' || $this->uri->segment(2) == 'features_tickets' ? 'active' : ''; ?>">
+				<a href="#" class="nav-link has-dropdown"><i class="fas fa-handshake"></i> <span>Transaksi</span></a>
+				<ul class="dropdown-menu">
+					<li class="<?php echo $this->uri->segment(2) == 'features_activities' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_activities">Pesanan</a></li>
+					<li class="<?php echo $this->uri->segment(2) == 'features_post_create' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_post_create">Produksi</a></li>
+					<li class="<?php echo $this->uri->segment(2) == 'features_posts' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_posts">Pembelian</a></li>
+				</ul>
+			</li>
+			<!-- ./transaksi -->
+			<li class="dropdown <?php echo $this->uri->segment(2) == 'features_activities' || $this->uri->segment(2) == 'features_post_create' || $this->uri->segment(2) == 'features_posts' || $this->uri->segment(2) == 'features_profile' || $this->uri->segment(2) == 'features_settings' || $this->uri->segment(2) == 'features_setting_detail' || $this->uri->segment(2) == 'features_tickets' ? 'active' : ''; ?>">
+				<a href="#" class="nav-link has-dropdown"><i class="fas fa-book"></i> <span>Laporan</span></a>
+				<ul class="dropdown-menu">
+					<li class="<?php echo $this->uri->segment(2) == 'features_activities' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_activities">Jurnal Umum</a></li>
+					<li class="<?php echo $this->uri->segment(2) == 'features_post_create' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_post_create">Buku Besar</a></li>
+					<li class="<?php echo $this->uri->segment(2) == 'features_posts' ? 'active' : ''; ?>"><a class="nav-link" href="<?php echo base_url(); ?>dist/features_posts">Harga Pokok Produksi</a></li>
+				</ul>
+			</li>
+		</ul>
+	</aside>
+</div>
---
+++ application/views/dashboard.php
@@ -0,0 +1,146 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+$this->load->view('_partials/header');
+?>
+<!-- Main Content -->
+<div class="main-content">
+	<section class="section">
+		<div class="section-header">
+			<h1>Dashboard</h1>
+		</div>
+		<div class="row">
+			<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-primary">
+						<i class="far fa-user"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Total Pelanggan</h4>
+						</div>
+						<div class="card-body">
+							10
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-danger">
+						<i class="fas fa-shopping-basket"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Total Pesanan</h4>
+						</div>
+						<div class="card-body">
+							42
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-success">
+						<i class="fas fa-check"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Pesanan Selesai</h4>
+						</div>
+						<div class="card-body">
+							1,201
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-3 col-md-6 col-sm-6 col-12">
+				<div class="card card-statistic-1">
+					<div class="card-icon bg-info">
+						<i class="fas fa-boxes"></i>
+					</div>
+					<div class="card-wrap">
+						<div class="card-header">
+							<h4>Daftar Produk</h4>
+						</div>
+						<div class="card-body">
+							47
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-lg-8 col-md-12 col-12 col-sm-12">
+				<div class="card">
+					<div class="card-header">
+						<h4>Daftar Pembelian Terbaru</h4>
+					</div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table class="table">
+								<thead>
+									<tr>
+										<th>#</th>
+										<th>No Transaksi</th>
+										<th>Status</th>
+										<th>Total</th>
+									</tr>
+								</thead>
+							</table>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-12 col-12 col-sm-12">
+				<div class="card">
+					<div class="card-header">
+						<h4>Daftar Pesanan Terbaru</h4>
+					</div>
+					<div class="card-body">
+						<ul class="list-unstyled list-unstyled-border">
+							<li class="media">
+								<img class="mr-3 rounded-circle" width="50" src="<?php echo base_url(); ?>assets/img/avatar/avatar-1.png" alt="avatar">
+								<div class="media-body">
+									<div class="float-right text-primary">Now</div>
+									<div class="media-title">Farhan A Mujib</div>
+									<span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</span>
+								</div>
+							</li>
+							<li class="media">
+								<img class="mr-3 rounded-circle" width="50" src="<?php echo base_url(); ?>assets/img/avatar/avatar-2.png" alt="avatar">
+								<div class="media-body">
+									<div class="float-right">12m</div>
+									<div class="media-title">Ujang Maman</div>
+									<span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</span>
+								</div>
+							</li>
+							<li class="media">
+								<img class="mr-3 rounded-circle" width="50" src="<?php echo base_url(); ?>assets/img/avatar/avatar-3.png" alt="avatar">
+								<div class="media-body">
+									<div class="float-right">17m</div>
+									<div class="media-title">Rizal Fakhri</div>
+									<span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</span>
+								</div>
+							</li>
+							<li class="media">
+								<img class="mr-3 rounded-circle" width="50" src="<?php echo base_url(); ?>assets/img/avatar/avatar-4.png" alt="avatar">
+								<div class="media-body">
+									<div class="float-right">21m</div>
+									<div class="media-title">Alfa Zulkarnain</div>
+									<span class="text-small text-muted">Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin.</span>
+								</div>
+							</li>
+						</ul>
+						<div class="text-center pt-1 pb-1">
+							<a href="#" class="btn btn-primary btn-lg btn-round">
+								View All
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
+<?php $this->load->view('_partials/footer'); ?>
